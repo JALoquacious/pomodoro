2ac98c0979ca77eae790afc2f907ce8afa456a99
Loquaciou5 <jim_likoudis@hotmail.com>
2016-08-23 10:37:31 -0400

    Add files via upload

--- logic.js
+++ logic.js
@@ -20,8 +20,9 @@ let sliderParams = {
             }
         }
     },
-    tick      = new Audio('sounds/tick.mp3'),
-    ding      = new Audio('sounds/ding.mp3'),
+    fileBase  = 'https://dl.dropboxusercontent.com/s/',
+    tick      = new Audio(fileBase + 'd4x41ou0q2jwmjg/tick.mp3'),
+    ding      = new Audio(fileBase + 'hjnhovbeq9r09lv/ding.mp3'),
     wrkTime   = sliderParams.start * 60,
     brkTime   = sliderParams.start * 60,
     wrkSlider = document.getElementById('slider-work'),
@@ -45,7 +46,7 @@ let sliderParams = {
 function slideRange() {
     let marks = {};
     [marks.min, marks.max] = [0, 20];
-    
+    // populate object with percentage values, min->max, every 5 steps
     for (let i = 0; i <= 100; i += 5 ) {
         marks[i + '%'] = i / 5;
     }
@@ -56,24 +57,25 @@ function slideRange() {
 // convert seconds to MM:SS format
 //---------------------------------------------------------------------
 function secs2MMSS(n) {
-	let m = Math.floor(n % (60 * 60) / 60),
-        s = Math.floor(n % (60 * 60) % 60);
+	let m = Math.floor(n % (60 * 60) / 60), // minutes
+        s = Math.floor(n % (60 * 60) % 60); // seconds
+    // if minute or second count less than 10, prepend zero
     return (m < 10 ? '0' : '') + m + ':' + (s < 10 ? '0' : '') + s;
 }//====================================================================
 
 
 // countdown & update display
 //---------------------------------------------------------------------
-function updateClock() {
+function updateClock() { // occurs at every interval
     timer--;
     tick.play();
-    clock.textContent  = secs2MMSS(timer);
-    progress.style.top = updateProgress();
+    clock.textContent  = secs2MMSS(timer); // seconds -> clock format
+    progress.style.top = updateProgress(); // move div bar from top
     
-    if (timer <= 0) {
+    if (timer <= 0) { // countdown finish
         ding.play();
-        timer = working ? brkTime : wrkTime;
-        working = !working;
+        timer = working ? brkTime : wrkTime; // alternate session timer
+        working = !working; // toggle work/break flag
         status.textContent = working ? wrkMsg : brkMsg;
     }
 }//====================================================================
@@ -83,9 +85,9 @@ function updateClock() {
 //---------------------------------------------------------------------
 function updateProgress() {
     return (
-        working
-            ? 0 + (timer / wrkTime)
-            : 1 - (timer / brkTime)
+        working // if work:
+            ? 0 + (timer / wrkTime) // divide to get %
+            : 1 - (timer / brkTime) // inverse % of above
     ) * 100 + '%';
 }//====================================================================
 
@@ -93,21 +95,20 @@ function updateProgress() {
 // handle timer 
 //---------------------------------------------------------------------
 function playPause() {
+    timer = motion ? timer : wrkTime; // enable continue after pause
+    status.textContent = working ? wrkMsg : brkMsg; // appropriate msg
+    
     if (wrkTime === 0) {
         window.alert("Work interval cannot start at 00:00");
-    }
-    
-    timer = motion ? timer : wrkTime;
-    status.textContent = working ? wrkMsg : brkMsg;
-    
-    if (!countdown && wrkTime > 0) {
+        reset();
+    } else if (!countdown && wrkTime > 0) { // before clock is started
         wrkSlider.setAttribute('disabled', true);
         brkSlider.setAttribute('disabled', true);
         countdown = window.setInterval(updateClock, 1000);
-        motion = true;
-    } else {
+        motion = true; // flag for play/pause button
+    } else { // if clock actively counting
         window.clearInterval(countdown);
-        countdown = 0;
+        countdown = 0; // remove count so 'if' statement reads false
     }
 }//====================================================================
 
@@ -128,7 +129,7 @@ function reset() {
 }//====================================================================
 
 
-//instantiate sliders
+// instantiate sliders
 noUiSlider.create(wrkSlider, sliderParams);
 noUiSlider.create(brkSlider, sliderParams);
 
@@ -137,7 +138,7 @@ noUiSlider.create(brkSlider, sliderParams);
 //---------------------------------------------------------------------
 wrkSlider.noUiSlider.on('update', function (time) {
     wrkTime = time * 60;
-    clock.textContent = secs2MMSS(wrkTime);
+    clock.textContent = secs2MMSS(wrkTime); // always start at 'work'
 });
 
 brkSlider.noUiSlider.on('update', function (time) {
